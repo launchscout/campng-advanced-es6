1544707b41b9f2661f247dfd98748dced935f7a8
Chris Nelson <superchrisnelson@gmail.com>
2015-10-12 16:22:44 -0400

    added bootstrap

--- client/app/app.js
+++ client/app/app.js
@@ -3,6 +3,8 @@ import uiRouter from 'angular-ui-router';
 import Common from './common/common';
 import Components from './components/components';
 import AppComponent from './app.component';
+
+import "bootstrap-webpack";
 import 'normalize.css';
 
 angular.module('app', [
