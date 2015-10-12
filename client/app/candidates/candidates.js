02e4da20740fee307ff2d99f690fdafa5801bfb3
Chris Nelson <superchrisnelson@gmail.com>
2015-10-12 15:06:32 -0400

    candidate query spec passes

--- client/app/candidates/candidates.js
+++ client/app/candidates/candidates.js
@@ -1,5 +1,8 @@
 import angular from 'angular';
+import CandidateService from './candidateService';
 
 let CandidatesModule = angular.module('candidates', []);
+
+CandidatesModule.service("CandidateService", CandidateService);
 
 export default CandidatesModule;
