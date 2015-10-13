0aaf165c2d944fe1357e73b48226a05370a45ee0
Chris Nelson <superchrisnelson@gmail.com>
2015-10-13 16:40:37 -0400

    New candidate works(ish)

--- client/app/candidates/candidates.js
+++ client/app/candidates/candidates.js
@@ -1,13 +1,22 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import showTemplate from './show.html'
+import newTemplate from './new.html';
 import CandidateService from './candidateService';
 import CandidateListController from './candidateList.controller';
 import ShowCandidateController from './show.controller';
+import NewCandidateController from './new.controller';
 
 let CandidatesModule = angular.module('candidates', [uiRouter])
 
 .config(($stateProvider) => {
+
+  $stateProvider.state('newCandidate', {
+    url: '/candidates/new',
+    template: newTemplate,
+    controller: NewCandidateController,
+    controllerAs: "vm"
+  });
   $stateProvider.state('showCandidate', {
     url: '/candidates/:id',
     template: showTemplate,
