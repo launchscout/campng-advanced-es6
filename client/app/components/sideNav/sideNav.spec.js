51182f77bb80d77343ed58c88fec77a9a1a1302c
Chris Nelson <superchrisnelson@gmail.com>
2015-10-13 17:49:26 -0400

    oops sideNav spec failure fixed.

--- client/app/components/sideNav/sideNav.spec.js
+++ client/app/components/sideNav/sideNav.spec.js
@@ -26,14 +26,6 @@ describe('SideNav', () => {
     });
   });
 
-  describe('Template', () => {
-    // template specs
-    // tip: use regex to ensure correct bindings are used e.g., {{  }}
-    it('has name in template [REMOVE]', () => {
-      expect(SideNavTemplate).to.match(/{{\s?vm\.name\s?}}/g);
-    });
-  });
-
   describe('Component', () => {
       // component/directive specs
       let component = SideNavComponent();
