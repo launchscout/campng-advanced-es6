6c184b654af1e3ef67a470ecc16ff213ba369a3a
Chris Nelson <superchrisnelson@gmail.com>
2015-10-13 10:56:27 -0400

    spec for candidates show

--- client/app/candidates/candidates.js
+++ client/app/candidates/candidates.js
@@ -1,8 +1,25 @@
 import angular from 'angular';
+import uiRouter from 'angular-ui-router';
+import showTemplate from './show.html'
 import CandidateService from './candidateService';
+import CandidateListController from './candidateList.controller';
+import ShowCandidateController from './show.controller';
 
-let CandidatesModule = angular.module('candidates', []);
+let CandidatesModule = angular.module('candidates', [uiRouter])
+
+.config(($stateProvider) => {
+  $stateProvider.state('showCandidate', {
+    url: '/candidates/:candidateId',
+    template: showTemplate,
+    controller: ShowCandidateController,
+    controllerAs: "vm"
+  });
+
+})
+
 
 CandidatesModule.service("CandidateService", CandidateService);
+
+CandidatesModule.controller("CandidateListController", CandidateListController);
 
 export default CandidatesModule;
