7c185d6d2e06f13660a4965481c7a1e0ab0e0eac
Chris Nelson <superchrisnelson@gmail.com>
2015-10-14 17:42:28 -0400

    use percentages for candidate ratings in server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ var express = require('express');
 var _ = require("underscore");
 
 var candidates = [
-  {id: 1, name: "John Doe", email: "john.doe@example.com", rating: 1},
-  {id: 2, name: "Jane Doe", email: "jane.doe@example.com", rating: 4},
+  {id: 1, name: "John Doe", email: "john.doe@example.com", rating: 40},
+  {id: 2, name: "Jane Doe", email: "jane.doe@example.com", rating: 60},
 ];
 
 function findCandidate(id) {
