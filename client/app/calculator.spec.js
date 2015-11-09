8cb92db7f8883a05138117aac95027678cc9a5d0
Chris Nelson <superchrisnelson@gmail.com>
2015-11-09 18:24:26 -0500

    better start point

--- client/app/calculator.spec.js
+++ client/app/calculator.spec.js
@@ -4,5 +4,15 @@ describe("Calculator", () => {
   it("should add two numbers", () => {
     let calc = new Calculator(4, 5);
     expect(calc.add()).to.equal(9);
-  })
+  });
+
+  it("should multiply two numbers", () => {
+  });
+
+  it("should subtract two numbers", () => {
+  });
+
+  it("should divide two numbers", () => {
+  });
+
 })
